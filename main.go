98ccd6a31381226429e522174d549c7a22a565e8
mo khan <mo@mokhan.ca>
2021-08-06 20:08:09 -0600

    add css and javascript

--- main.go
+++ main.go
@@ -9,6 +9,9 @@ import (
 func setupRouter() *gin.Engine {
 	router := gin.Default()
 	router.StaticFile("/", "./public/index.html")
+	router.StaticFile("/favicon.ico", "./public/favicon.ico")
+	router.StaticFile("/application.js", "./public/application.js")
+	router.StaticFile("/application.css", "./public/application.css")
 
 	router.GET("/ping", func(context *gin.Context) {
 		context.JSON(http.StatusOK, gin.H{"message": "pong"})
