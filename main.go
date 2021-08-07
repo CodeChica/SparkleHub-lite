87ea4ede2d29557416f06a7b64a4408d0cb5884c
mo khan <mo@mokhan.ca>
2021-08-06 22:09:55 -0600

    fix: update create endpoint to save sparkles in memory

--- main.go
+++ main.go
@@ -2,17 +2,53 @@ package main
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
 )
 
+type User struct {
+	Name string
+}
+
+type Sparkle struct {
+	Sparklee User
+	Sparkler User
+	Reason   string
+}
+
+type SparkleRepository struct {
+	Sparkles []Sparkle
+}
+
+func (self *SparkleRepository) Insert(sparkler string, sparklee string, reason string) {
+	self.Sparkles = append(self.Sparkles, Sparkle{
+		Sparklee: User{Name: sparklee},
+		Sparkler: User{Name: sparkler},
+		Reason:   reason,
+	})
+}
+
+var db SparkleRepository
+
 func setupRouter() *gin.Engine {
 	router := gin.Default()
 	router.Use(static.Serve("/", static.LocalFile("./public", true)))
 
+	db.Insert("xlgmokha", "kirsty", "for volunteering")
+
 	router.GET("/ping", func(context *gin.Context) {
 		context.JSON(http.StatusOK, gin.H{"message": "pong"})
+	})
+	router.GET("/sparkles", func(context *gin.Context) {
+		context.JSON(http.StatusOK, gin.H{"sparkles": db.Sparkles})
+	})
+	router.POST("/sparkles", func(context *gin.Context) {
+		body := context.PostForm("body")
+		items := strings.SplitAfterN(body, " ", 2)
+		db.Insert("xlgmokha", items[0], items[1])
+		context.Redirect(http.StatusFound, "/")
 	})
 	return router
 }
