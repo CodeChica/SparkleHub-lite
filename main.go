ee6a355a6049e2b046789816599651d810c27407
mo khan <mo@mokhan.ca>
2021-08-06 20:22:31 -0600

    fix: serve all static files from public dir

--- main.go
+++ main.go
@@ -3,15 +3,13 @@ package main
 import (
 	"net/http"
 
+	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
 )
 
 func setupRouter() *gin.Engine {
 	router := gin.Default()
-	router.StaticFile("/", "./public/index.html")
-	router.StaticFile("/favicon.ico", "./public/favicon.ico")
-	router.StaticFile("/application.js", "./public/application.js")
-	router.StaticFile("/application.css", "./public/application.css")
+	router.Use(static.Serve("/", static.LocalFile("./public", true)))
 
 	router.GET("/ping", func(context *gin.Context) {
 		context.JSON(http.StatusOK, gin.H{"message": "pong"})
