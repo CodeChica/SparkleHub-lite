19ad1f6b9544492939e3c3050483dc7a151cbf6a
mo khan <mo@mokhan.ca>
2021-08-28 16:06:21 -0600

    feat: poll server for new sparkles

--- public/application.js
+++ public/application.js
@@ -1,5 +1,33 @@
-fetch("/sparkles.html")
-  .then((response) => response.text())
-  .then((html) => {
-    document.querySelector('#sparkles-list').innerHTML = html
-  });
+class Sparkles {
+  constructor(id, url) {
+    this.element = document.querySelector(id);
+    this.url = url;
+  }
+
+  refresh() {
+    fetch(this.url)
+      .then((response) => response.text())
+      .then((html) => this.element.innerHTML = html);
+  }
+
+  startPolling() {
+    if (!this.intervalId) {
+      this.intervalId = setInterval(() => this.refresh(), 1000);
+    }
+  }
+
+  stopPolling() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  window.Sparkles = new Sparkles('#sparkles-list', "/sparkles.html")
+  window.Sparkles.startPolling();
+});
+
+document.addEventListener('unload', (event) => {
+  window.Sparkles.stopPolling();
+});
