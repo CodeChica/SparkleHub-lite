7c8043881cdba9cb847e8957f57397849ded0aa8
mo khan <mo@mokhan.ca>
2021-09-14 19:31:54 -0600

    test: split tests based on route

--- main_test.go
+++ main_test.go
@@ -11,34 +11,55 @@ import (
 
 const NoSparklesMessage string = "No sparkles yet"
 
-func TestWhenThereAreNoSparkles(t *testing.T) {
-	sparkles := []Sparkle{}
-	router := setupRouter(&sparkles)
+func TestServer(t *testing.T) {
+	t.Run("GET /sparkles.html", func(t *testing.T) {
+		t.Run("when there are no sparkles", func(t *testing.T) {
+			sparkles := []Sparkle{}
+			router := setupRouter(&sparkles)
 
-	response := httptest.NewRecorder()
-	request, _ := http.NewRequest("GET", "/sparkles.html", nil)
-	router.ServeHTTP(response, request)
+			response := httptest.NewRecorder()
+			request, _ := http.NewRequest("GET", "/sparkles.html", nil)
+			router.ServeHTTP(response, request)
 
-	assert.Equal(t, 200, response.Code)
-	assert.Contains(t, response.Body.String(), NoSparklesMessage)
-}
+			assert.Equal(t, 200, response.Code)
+			assert.Contains(t, response.Body.String(), NoSparklesMessage)
+		})
 
-func TestWhenOneSparkleIsCreated(t *testing.T) {
-	sparkles := []Sparkle{}
-	router := setupRouter(&sparkles)
+		t.Run("when there is a single sparkle", func(t *testing.T) {
+			sparkles := []Sparkle{
+				{
+					Sparklee: "@monalisa",
+					Reason:   "for being kind",
+				},
+			}
+			router := setupRouter(&sparkles)
 
-	response := httptest.NewRecorder()
-	request, _ := http.NewRequest("POST", "/sparkles", strings.NewReader("body=@monalisa+for+being+kind"))
-	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	router.ServeHTTP(response, request)
+			response := httptest.NewRecorder()
+			request, _ := http.NewRequest("GET", "/sparkles.html", nil)
+			router.ServeHTTP(response, request)
 
-	assert.Equal(t, 302, response.Code)
+			assert.Equal(t, 200, response.Code)
+			assert.NotContains(t, response.Body.String(), NoSparklesMessage)
+			assert.Contains(t, response.Body.String(), "for being kind")
+		})
+	})
 
-	response = httptest.NewRecorder()
-	request, _ = http.NewRequest("GET", "/sparkles.html", nil)
-	router.ServeHTTP(response, request)
+	t.Run("POST /sparkles", func(t *testing.T) {
+		t.Run("with valid data", func(t *testing.T) {
+			sparkles := []Sparkle{}
+			router := setupRouter(&sparkles)
 
-	assert.Equal(t, 200, response.Code)
-	assert.NotContains(t, response.Body.String(), NoSparklesMessage)
-	assert.Contains(t, response.Body.String(), "for being kind")
+			response := httptest.NewRecorder()
+			request, _ := http.NewRequest("POST", "/sparkles", strings.NewReader("body=@monalisa+for+being+kind"))
+			request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+			router.ServeHTTP(response, request)
+
+			assert.Equal(t, 302, response.Code)
+			assert.Equal(t, 1, len(sparkles))
+
+			sparkle := sparkles[0]
+			assert.Equal(t, "@monalisa", sparkle.Sparklee)
+			assert.Equal(t, "for being kind", sparkle.Reason)
+		})
+	})
 }
