f3da378e386377ceccaa742dee06c47b33a84736
mo khan <mo@mokhan.ca>
2021-09-16 10:55:51 -0600

    test: test the JSON endpoint

--- server_test.go
+++ server_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -21,7 +22,7 @@ func TestServer(t *testing.T) {
 			request, _ := http.NewRequest("GET", "/sparkles.html", nil)
 			server.ServeHTTP(response, request)
 
-			assert.Equal(t, 200, response.Code)
+			assert.Equal(t, http.StatusOK, response.Code)
 			assert.Contains(t, response.Body.String(), NoSparklesMessage)
 		})
 
@@ -38,9 +39,26 @@ func TestServer(t *testing.T) {
 			request, _ := http.NewRequest("GET", "/sparkles.html", nil)
 			server.ServeHTTP(response, request)
 
-			assert.Equal(t, 200, response.Code)
+			assert.Equal(t, http.StatusOK, response.Code)
 			assert.NotContains(t, response.Body.String(), NoSparklesMessage)
 			assert.Contains(t, response.Body.String(), "for being kind")
+		})
+	})
+
+	t.Run("GET /sparkles.json", func(t *testing.T) {
+		t.Run("with valid data", func(t *testing.T) {
+			sparkles := []Sparkle{{Sparklee: "@monalisa", Reason: "for helping me with my homework."}}
+
+			response := httptest.NewRecorder()
+			request, _ := http.NewRequest("GET", "/sparkles.json", nil)
+			NewServer(&sparkles).ServeHTTP(response, request)
+
+			assert.Equal(t, http.StatusOK, response.Code)
+			var got map[string][]Sparkle
+			assert.Nil(t, json.NewDecoder(response.Body).Decode(&got))
+			assert.Equal(t, 1, len(got["sparkles"]))
+			assert.Equal(t, "@monalisa", got["sparkles"][0].Sparklee)
+			assert.Equal(t, "for helping me with my homework.", got["sparkles"][0].Reason)
 		})
 	})
 
